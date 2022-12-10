b3967cd14fd25393f9b5c3e8287c7a28bc96cc2b
zachuri <punsalang.zachary@gmail.com>
2022-12-09 18:57:05 -0800

    drawer navigator: added about screen

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import MainScreen from './screens/main-screen'
 import Sidebar from './components/sidebar'
+import AboutScreen from './screens/about-screen'
 // import AboutScreen from './screens/about-screen'
 // import Sidebar from './components/sidebar'
 
@@ -20,7 +21,7 @@ const App = () => {
       }}
     >
       <Drawer.Screen name="Main" component={MainScreen} />
-      {/* <Drawer.Screen name="About" component={AboutScreen} /> */}
+      <Drawer.Screen name="About" component={AboutScreen} />
     </Drawer.Navigator>
   )
 }
