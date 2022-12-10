d4c2e1c9d977bfc32ba220cb6a8139a62ecfbb37
zachuri <punsalang.zachary@gmail.com>
2022-12-09 18:48:44 -0800

    drawer: setup drawer navigator with main screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import AppContainer from './src/components/app-container'
-import Main from './src/screens/main'
+import Navigator from './src/index'
 
 export default function App() {
   return (
     <AppContainer>
-      <Main />
+      <Navigator />
     </AppContainer>
   )
 }
